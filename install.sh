e0d898c4cb82c1b130a3c7ebfe203f5f90258a0d
NeiAozora <noxindocraft@gmail.com>
2025-03-07 23:27:35 +0700

    patch script

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ sudo apt-get install -y liblapack-dev libblas-dev gfortran libfreetype6-dev libo
 pip install -r requirements.txt
 
 wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
+pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+
+wget https://github.com/pytorch/vision/archive/refs/tags/v0.11.1.tar.gz -O torchvision-0.11.1.tar.gz
+
+mkdir -p torchvision && tar -xzf "torchvision-0.11.1.tar.gz" --strip-components=1 -C torchvision
+
 cd torchvision
 sudo python3 setup.py install 
