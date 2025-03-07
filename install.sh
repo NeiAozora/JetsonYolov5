4d0bfc745824c8070d49d3feae6c0b8ca626b832
NeiAozora <noxindocraft@gmail.com>
2025-03-07 22:57:11 +0700

    added extra helpers

---
+++ install.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get install -y liblapack-dev libblas-dev gfortran libfreetype6-dev libopenblas-base libopenmpi-dev libjpeg-dev zlib1g-dev
+
+pip install -r requirements.txt
+
+wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+pip3 install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+git clone --branch v0.11.1 https://github.com/pytorch/vision torchvision
+cd torchvision
+sudo python3 setup.py install 
